98518a32eb0c0e969db19507cbc0e43c37572ed2
Joubert Guimarães de Assis <me+github@redrat.com.br>
2020-04-13 20:49:40 -0300

    ex 134 - create tab header component

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -5,7 +5,7 @@ import Content from '../common/template/content'
 import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
-
+import TabHeader from '../common/tab/tabHeader'
 
 class BillingCycle extends Component {
   render() {
@@ -15,7 +15,10 @@ class BillingCycle extends Component {
         <Content>
           <Tabs>
             <TabsHeader>
-
+              <TabHeader label='Listar' icon='bars' target='tab-list' />
+              <TabHeader label='Incluir' icon='plus' target='tab-create' />
+              <TabHeader label='Alterar' icon='pencil' target='tab-update' />
+              <TabHeader label='Excluir' icon='trash-o' target='tab-delete' />
             </TabsHeader>
             <TabsContent>
 
